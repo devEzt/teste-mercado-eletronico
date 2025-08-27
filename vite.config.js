6e5da10b34788c02b0a83716559c1c7c6e2578f8
AlexandreDev <alexandreha.maciel@gmail.com>
2025-08-27 02:48:43 -0300

    chore: Define server.port=3000 e pequenos ajustes de config

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,14 @@ import vue from '@vitejs/plugin-vue'
 export default defineConfig({
   plugins: [vue()],
   server: {
-    port: 3000
+    port: 3000,
+    proxy: {
+      '/api': {
+        target: 'https://api.mercadoe.space',
+        changeOrigin: true,
+        secure: true,
+        rewrite: (path) => path.replace(/^\/api/, '')
+      }
+    }
   }
 })
